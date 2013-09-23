7968c32d699897d8b2cfb00804ec763aad444f7f
Roberto Arista <arista.rob@gmail.com>
2013-09-23 21:14:11 +0200

    Script name changed

--- scripts/typeStats/typeStats.py
+++ scripts/typeStats/typeStats.py
@@ -24,9 +24,9 @@ THE SOFTWARE.
 ### Importazione moduli esterni
 import robofab
 from robofab.world import *
-from robofab.pens.filterPen import flattenGlyph
+from robofab.pens.filterPen import flattenGlyph, _estimateCubicCurveLength
 from operator import itemgetter
-from math import fabs
+from math import fabs, hypot
 import sys
 
 ### Classi e funzioni
@@ -44,6 +44,26 @@ class typeStats:
             else:
                 d[i] = 1
         return d
+        
+    # Funzione che copia il glifo in una nuova cassa e lo appiattisce
+    def copyAndFlat(self, glyphName, font, precision):
+        
+        # Apertura glifo
+        glyph = font[glyphName]
+        
+        # Creazione di una cassa temporanea
+        temp_font = robofab.world.RFont(showUI = False)
+        
+        # Copia del glifo in un'altra font
+        glyph_matrix = glyph.copy()
+        temp_font.insertGlyph(glyph_matrix)
+        
+        # Appiattimento della copia
+        glyph_copy = temp_font[glyphName]
+        flattenGlyph(glyph_copy, precision)
+        
+        return glyph_copy
+    
 
     # Funzione per il calcolo dell'xHeight minuscola
     def xHeight(self, font, case):
@@ -88,20 +108,8 @@ class typeStats:
     # Definizione della funzione in grado di estrapolare il peso di un carattere
     def weight(self, font):
         
-        # Apertura glifo
-        l = font['l']
-        
-        # Creazione di una cassa temporanea
-        temp_font = robofab.world.RFont(showUI = False)
-        
-        # Copia del glifo in un'altra font
-        l_matrix = l.copy()
-        temp_font.insertGlyph(l_matrix)
-        
-        # Appiattimento della copia
-        l_copy = temp_font['l']
-        flattenGlyph(l_copy)
-        
+        # Copia appiattita su cui fare le misurazioni
+        l_copy = stats.copyAndFlat('l', font, 10)
         
         # Raccolta delle ascisse in una lista
         list_x = []
@@ -123,6 +131,86 @@ class typeStats:
         weight = fabs(estremo_2 + estremo_1)/stats.xHeight(font, 'lowercase')
         
         return weight
+        
+        
+    # Funzione in grado di estrarre il valore di contrasto da un carattere
+    def contrast(self, contrast):
+        
+        # Copia appiattita su cui fare le misurazioni
+        o_copy = stats.copyAndFlat('o', font, 10)
+        
+        # Creazione delle liste in cui collezionare i punti dei contorni
+        lista_punti_esterno = []
+        lista_punti_interno = []
+        
+        # Valutazione automatica interno/esterno
+        len_0 = len(o_copy[0].points)
+        len_1 = len(o_copy[1].points)
+        
+        # Creazione livelli interno/esterno
+        livello_esterno = o_copy.getLayer('esterno', clear=True)
+        livello_interno = o_copy.getLayer('interno', clear=True)
+        
+        # Travaso contorni
+        if len_0 > len_1:
+            # Copia coordinate punti per calcolo distanza minore
+            contorno_esterno = o_copy[0].points
+            contorno_interno = o_copy[1].points
+            
+            # Copia contorni nei livelli di destinazione
+            livello_esterno.appendContour(o_copy[0])
+            livello_interno.appendContour(o_copy[1])
+            
+        elif len_0 < len_1:
+            # Copia coordinate punti per calcolo distanza minore
+            contorno_esterno = o_copy[1].points
+            contorno_interno = o_copy[0].points
+            
+            # Copia contorni nei livelli di destinazione
+            livello_esterno.appendContour(o_copy[1])
+            livello_interno.appendContour(o_copy[0])
+        
+        else:
+            print "Errore"
+            sys.exit()
+            
+        ## Calcolo spessore minore
+        # Ciclo nidiato che itera sui punti di entrambi i contorni
+        distanze = []
+        for pt_est in contorno_esterno:
+            for pt_int in contorno_interno:
+                
+                # Calcolo della distanza sul piano cartesiano
+                distanza = hypot(pt_est.x - pt_int.x, pt_est.y - pt_int.y)
+                
+                # Inserimento della distanza calcolata all'interno di una lista
+                distanze.append(distanza)
+        
+        # Riordinamento lista distanze
+        distanze.sort()
+        
+        # Spessore minimo della 'o'
+        spessore_minimo = distanze[0]
+        
+        ## Calcolo spessore maggiore
+        
+        # Appiattimento livello con contorno interno
+        flattenGlyph(livello_interno, 10)
+        
+        print livello_esterno[0].length
+        
+        
+        #for con in o_copy:
+        #    len_1 = len(con.points)
+        #    for pt in con.points:
+                
+                
+        #        print pt.x, pt.y
+        
+        
+        
+        contrast = 10
+        return contrast
 
 
 ### Variabili
@@ -134,12 +222,17 @@ stats = typeStats()
 
 # xHeight minuscola
 xHeight_lowercase = stats.xHeight(font, 'lowercase')
-print xHeight_lowercase
+print "xHeight:\t", xHeight_lowercase
 
 # xHeight maiuscola
 xHeight_uppercase = stats.xHeight(font, 'uppercase')
-print xHeight_uppercase
+print "XHeight:\t", xHeight_uppercase
 
 # Peso del carattere 
 weight = stats.weight(font)
-print weight
+print "Peso:\t\t", weight
+
+# Contrasto del carattere
+contrasto = stats.contrast(font)
+print "Contrato:\t", contrasto
+
